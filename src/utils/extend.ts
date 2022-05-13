ef9c389e129df81416f31bf2432433905387ddd5
tachibana-shin <45375496+tachibana-shin@users.noreply.github.com>
2022-05-13 01:27:43 +0000

    fix extend not deep

--- src/utils/extend.ts
+++ src/utils/extend.ts
@@ -4,13 +4,13 @@ export default function extend<T>(target: T, deep = 1, ...objs: any): any {
   objs.forEach((obj: any) => {
     for (const prop in obj) {
       if (
+        deep > 0 &&
         typeof (target as any)[prop] === "object" &&
         (target as any)[prop] !== null &&
         typeof obj[prop] === "object" &&
-        obj[prop] !== null &&
-        deep > 0
+        obj[prop] !== null
       ) {
-        extend((target as any)[prop], obj[prop])
+        extend((target as any)[prop], deep - 1, obj[prop])
       }
 
       // eslint-disable-next-line functional/immutable-data
